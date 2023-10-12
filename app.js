339fa571c114b1d10387439179fafc187c0685f6
EL5225 <luthfiramadhan155@gmail.com>
2023-10-12 07:40:37 +0700

    feature: add docs

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from "express";
 import "dotenv/config";
+import fs from "fs";
+import YAML from "yaml";
+import swaggerUi from "swagger-ui-express";
 import router from "./routes/v1/index.js";
-// import user from "./handlers/v1/user.js";
 import { errorHandler, notFoundHandler } from "./middlewares/index.js";
+import cors from "cors";
 
 const app = new express();
 const { PORT } = process.env;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", router);
+
+const file = fs.readFileSync("./docs/swagger.yaml", "utf-8");
+const swaggerDocument = YAML.parse(file);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(notFoundHandler);
 app.use(errorHandler);
